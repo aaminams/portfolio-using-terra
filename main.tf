95185ced947b01821172de8afcf174a572c3aa7e
aamina <133568969+aaminams@users.noreply.github.com>
2026-06-12 19:45:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,47 @@
+########################################
+# VPC
+########################################
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+########################################
+# Public Subnet
+########################################
 resource "aws_subnet" "public" {
-  vpc_id                  = "vpc-00235529d5191284d"
+  vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
 
   availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "public-subnet"
+  }
 }
 
+########################################
+# Internet Gateway
+########################################
 resource "aws_internet_gateway" "igw" {
-  vpc_id = "vpc-00235529d5191284d"
+  vpc_id = aws_vpc.main.id
 
   tags = {
     Name = "main-igw"
   }
 }
 
+########################################
+# Route Table (Public)
+########################################
 resource "aws_route_table" "public_rt" {
-  vpc_id = "vpc-00235529d5191284d"
+  vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -27,15 +53,21 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
+########################################
+# Route Table Association
+########################################
 resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public_rt.id
 }
 
+########################################
+# Security Group
+########################################
 resource "aws_security_group" "sg" {
   name        = "basic-sg"
   description = "Allow HTTP"
-  vpc_id      = "vpc-00235529d5191284d"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     from_port   = 80
@@ -50,24 +82,37 @@ resource "aws_security_group" "sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "basic-sg"
+  }
 }
+
+########################################
+# EC2 Instance
+########################################
 resource "aws_instance" "web" {
   ami           = "ami-091138d0f0d41ff90"
   instance_type = "t2.micro"
+
   subnet_id = aws_subnet.public.id
 
   vpc_security_group_ids = [
     aws_security_group.sg.id
   ]
 
-  user_data     = file("userdata.sh")
+  user_data = file("userdata.sh")
 
   tags = {
     Name = "basic-terra"
   }
 }
 
+########################################
+# Output
+########################################
 output "instance_public_ip" {
   value       = aws_instance.web.public_ip
-  description = "Website is running on this address:"
+  description = "Website is running on this address"
+}
 }
